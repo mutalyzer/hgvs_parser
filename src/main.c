8cb7898501c7b427bd34c0b081256665959c37b2
jkvis <j.k.vis@lumc.nl>
2019-02-18 17:09:26 +0100

    Refactoring

---
+++ src/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+#include "../include/hgvs_parser.h"
+
+
+int
+main(int argc, char* argv[])
+{
+    if (argc <= 1)
+    {
+        fprintf(stderr, "Usage: %s string\n", argv[0]);
+        return EXIT_FAILURE;
+    } // if
+
+    HGVS_Node* root = HGVS_parse(argv[1]);
+    if (root == NULL)
+    {
+        fprintf(stderr, "failed.\n");
+        return EXIT_FAILURE;
+    } // if
+
+    HGVS_Node_destroy(root);
+
+    fprintf(stderr, "success\n");
+    return EXIT_SUCCESS;
+} // main
